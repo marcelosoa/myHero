f856d61b005dda1ee57f2518437e5433436aa4e1
Marcelo Soares <itzcello@live.com>
2023-06-14 14:29:30 -0300

    feat: register-screen

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import RegisterScreen from './components/screens/register';
 import RadioComponent from './components/Radio/index';
 import HomeScreen from './components/screens/home';
+import LoginScreen from './components/screens/login';
 
 function App() {
-  return <RegisterScreen />;
+  return <LoginScreen />;
 }
 
 export default App;
--- components/screens/register/index.js
+++ components/screens/register/index.js
@@ -1,7 +1,14 @@
-import { SafeAreaView, StyleSheet, TouchableOpacity, Text, Image } from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  Image,
+  KeyboardAvoidingView,
+} from 'react-native';
 import InputComponent from '../../Input';
 import RadioComponent from '../../Radio';
-import { useState } from 'react';
+import {useState} from 'react';
 
 export default function RegisterScreen() {
   const [name, setName] = useState('');
@@ -10,33 +17,37 @@ export default function RegisterScreen() {
   const [idC, setidC] = useState('');
   const [selected, setSelected] = useState(0);
 
-  function handleRegister() {
-
-  }
+  function handleRegister() {}
   return (
-    <SafeAreaView style={{
-      flex: 1,
-      justifyContent: 'center',
-    }}>
-      <Image style={styles.image} source={require('../../../assets/images/walking2.png')} />
-  <InputComponent placeholder="nome completo"/>
-  <InputComponent placeholder="senha"/>
-  <InputComponent placeholder="confirmar senha"/>
-  <InputComponent placeholder="email"/>
-  <InputComponent placeholder="CPF/CNPJ"/>
-  <RadioComponent
-  selected={selected}
-  options={['CPF', 'CNPJ']}
-  horizontal={true}
-  onChangeSelect={(option, index) => {
-    setSelected(index);
-  }}
+    <SafeAreaView
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+      }}>
+      <KeyboardAvoidingView>
+        <Image
+          style={styles.image}
+          source={require('../../../assets/images/walking2.png')}
+        />
+        <InputComponent placeholder="nome completo" />
+        <InputComponent placeholder="senha" />
+        <InputComponent placeholder="confirmar senha" />
+        <InputComponent placeholder="email" />
+        <InputComponent placeholder="CPF/CNPJ" />
+        <RadioComponent
+        selected={selected}
+        options={['CPF', 'CNPJ']}
+        horizontal={true}
+        onChangeSelect={(option, index) => {
+          setSelected(index);
+        }}
+      />
+      </KeyboardAvoidingView>
 
-  />
-  <TouchableOpacity onPress={handleRegister} style={styles.textInput}>
-    <Text style={styles.text}>Cadastrar-se</Text>
-  </TouchableOpacity>
-  </SafeAreaView>
+      <TouchableOpacity onPress={handleRegister} style={styles.textInput}>
+        <Text style={styles.text}>Cadastrar-se</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 }
 
@@ -46,9 +57,9 @@ const styles = StyleSheet.create({
     marginTop: 20,
     paddingLeft: 20,
     marginHorizontal: 20,
-    borderRadius: 9,
+    borderRadius: 8,
     backgroundColor: '#CBB279',
-    borderColor: '##CBB279',
+    borderColor: '#fff',
     borderWidth: 1,
     display: 'flex',
     justifyContent: 'center',
