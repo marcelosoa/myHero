c9cfc4c0d11866cab20880b7d896dae8334ceeb8
Marcelo Soares <itzcello@live.com>
2023-06-14 22:58:30 -0300

    changelog

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import RegisterScreen from './components/screens/register';
-import RadioComponent from './components/Radio/index';
 import HomeScreen from './components/screens/home';
 import LoginScreen from './components/screens/login';
 
 function App() {
-  return <HomeScreen />;
+  return <LoginScreen />;
 }
 
 export default App;
